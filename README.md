3e75e6bcd9ab136beda79be9056d6d7a8248fa73
Rai Batista <142160722+raibatista@users.noreply.github.com>
2023-10-15 19:42:43 +0000

    adcionar script `lint:check` e lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementacao do https://www.tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return<h1>Eu sou o responsavel pela minha vida </h1>
-    
+  return <h1>Eu sou o responsavel pela minha vida </h1>;
 }
 
 export default Home;
