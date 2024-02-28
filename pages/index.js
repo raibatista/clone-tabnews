1be961d6fcd5c1b22d71c72a40e334137d4031c7
Rai Batista <142160722+raibatista@users.noreply.github.com>
2024-02-28 02:15:25 +0000

    qmodificacao de pagina 2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
+import { loadStaticPaths } from "next/dist/server/dev/static-paths-worker";
+
 function Home() {
   return <h1>Eu sou o responsavel pela minha vida </h1>;
 }
 
 export default Home;
+loadStaticPaths
